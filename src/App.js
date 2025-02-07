dafa68e473f7c4885826e1f97ffcd28ab2e9e256
Heather Smith <halexsmith86@gmail.com>
2025-02-07 13:52:48 -0500

    installed chakraui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import { Box, VStack } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <VStack>
+          <p>Meet Your New Best Friend</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </VStack>
       </header>
     </div>
   );
